8365f0e3e766eb947f3d5c0a0362e596bc80d5d1
tasniaarhamarham <tasniaarhamarham@gmail.com>
2024-05-07 15:16:14 +0600

    last work

--- app/(site)/home3/page.js
+++ app/(site)/home3/page.js
@@ -6,31 +6,32 @@ import { VscSend } from "react-icons/vsc";
 const page = () => {
   return (
     <div className=" md:pt-[86.99px] pt-[40px] md:pb-[100px] pb-[50px] bg-[url('/bg.png')] bg-cover bg-no-repeat">
-      <div className=" h-[581px] relative  ">
-        <div className="w-[50%]">
+      <div className=" h-[634px] relative ">
+        <div className="absolute sm:block hidden right-0 w-[50%] h-full img-shape">
         <Image
           src="/driver.png"
           width={1101}
           height={634}
           alt="banner"
-          className="absolute sm:block hidden  z-40 right-0  h-full img-shape "
+          className=" w-full h-full"
         />
-         <Image
+      <Image
           src="/frame.png"
           width={1101}
           height={634}
           alt="banner"
-          className="absolute sm:block hidden z-30   right-[53px] top-[-53px] h-[634px] img-shape "
+          className=" w-full h-full"
         />
         </div>
-      
+       
 
-          {/* <div className=" container  lg:items-center  flex lg:flex-row flex-col  justify-center ">
-          <div className=" sm:w-[50%] w-full h-full">
+        <div className=" container  flex lg:flex-row flex-col items-center  justify-center ">
+          <div className="sm:w-[50%] w-full    h-full">
             <div className="xl:text-[61px] text-[30px] text-white font-bold sm:mb-[32px] mb-[16px] capitalize">
               <h2>book your taxi from </h2>
               <h2 className=" text-[#1F8A70]"> anywhere today!</h2>
             </div>
+
             <p className="text-white font-normal  w-full sm:mb-[40px] mb-[20px] sm:text-[16px] text-[10px] ">
               A taxi is a motor vehicle designed primarily for the
               transportation of passengers for a fare. It is often characterized
@@ -51,23 +52,24 @@ const page = () => {
               Book Ride
             </button>
           </div>
-          <div className="flex lg:h-[634px] md:h-[280px] sm:h-[260px] flex-col sm:justify-end justify-start xl:w-auto w-full ">
-            <div className="grid  grid-cols-3 sm:gap-x-0 gap-x-3 justify-items-center   w-full  lg:z-50  lg:mb-[-37px] sm:gap-y-0 mt-4 gap-y-4">
-              <div className="sm:w-[209px] w-full  sm:h-[74px] bg-[#1F8A70] text-white text-center flex flex-col items-center img-shape2">
-                <h3 className="font-medium text-[24px]">8k+</h3>
-                <h6 className="font-normal text-[#DEDEDE]">App Downloads</h6>
-              </div>
-              <div className="sm:w-[209px] w-full  sm:h-[74px] bg-[#1F8A70] text-white text-center flex flex-col items-center img-shape2">
-                <h3 className="font-medium text-[24px]">8k+</h3>
-                <h6 className="font-normal text-[#DEDEDE]">Active Rider</h6>
-              </div>
-              <div className="sm:w-[209px] w-full  sm:h-[74px] bg-[#1F8A70] text-white text-center flex flex-col items-center img-shape2">
-                <h3 className="font-medium text-[24px]">8k+</h3>
-                <h6 className="font-normal text-[#DEDEDE]">Active User</h6>
-              </div>
+          <div className="flex lg:h-[634px] md:h-[280px] sm:h-[260px] flex-col sm:justify-end justify-start xl:w-auto ">
+          <div className="grid  grid-cols-3 sm:gap-x-0 gap-x-3 justify-items-center lg::justify-items-start  w-full  lg:z-40  lg:mb-[-37px] sm:gap-y-0 mt-4 gap-y-4">
+            <div className="sm:w-[209px] w-full  sm:h-[74px] bg-[#1F8A70] text-white text-center flex flex-col items-center img-shape2">
+              <h3 className="font-medium text-[24px]">8k+</h3>
+              <h6 className="font-normal text-[#DEDEDE]">App Downloads</h6>
+            </div>
+            <div className="sm:w-[209px] w-full  sm:h-[74px] bg-[#1F8A70] text-white text-center flex flex-col items-center img-shape2">
+              <h3 className="font-medium text-[24px]">8k+</h3>
+              <h6 className="font-normal text-[#DEDEDE]">Active Rider</h6>
+            </div>
+            <div className="sm:w-[209px] w-full  sm:h-[74px] bg-[#1F8A70] text-white text-center flex flex-col items-center img-shape2">
+              <h3 className="font-medium text-[24px]">8k+</h3>
+              <h6 className="font-normal text-[#DEDEDE]">Active User</h6>
             </div>
           </div>
-        </div> */}
+          </div>
+        
+        </div>
       </div>
     </div>
   );
@@ -75,3 +77,48 @@ const page = () => {
 
 export default page;
 
+// <div className="bg-[url('/banner.png')] bg-cover bg-no-repeat pt-[79px]    pb-[100px]  z-10 relative">
+//     <div className="w-[60%] absolute right-0">
+//         <div className="w-full relative">
+//           <div className="md:max-w-full md:max-h-[634px] absolute  ">
+//             <img src="man_bg.png" alt="bg" className="h-full w-auto" />
+//           </div>
+//           <div className="md:max-w-full md:max-h-[581px] absolute">
+//             <img src="/man.png" className="w-auto h-full" alt="man" />
+//           </div>
+//           </div>
+//         </div>
+//  <div className="flex  container">
+//         <div className="w-2/5 pt-[22px]  lg:pb-[100px] pb-[50px]">
+//           <Image
+//             src="/car_svg.png"
+//             className="h-[257px] md:w-[477px] w-full absolute lg:left-[300px] mt-[64px] object-fill md:block hidden "
+//              height={257}
+//              width={477}
+//            />
+//            <h2 className="xl:text-[61px] md:text-[38px] sm:text-start text-center text-[28px] text-white font-bold mb-[32px] capitalize">
+//       book your taxi from{" "}
+//       <span className="ms-2 text-[#1F8A70]">anywhere today!</span>
+//     </h2>
+//     <p className="text-white font-normal  w-full mb-[40px] sm:text-[16px] text-[12px]">
+//       A taxi is a motor vehicle designed primarily for the
+//       transportation of passengers for a fare. It is often
+//       characterized by its distinctive exterior markings,
+//     </p>
+//     <div className="flex gap-x-[16px]">
+//       <Input
+//         placeholder="Pickup location"
+//         className="md:py-[12px] md:px-[20px] py-[10px] px-[8px]"
+//         suffix={<VscSend />}
+//       />
+//       <Input
+//         placeholder="Enter destination"
+//         className="md:py-[12px] md:px-[20px] py-[10px] px-[8px]"
+//       />
+//     </div>
+//     <button className="text-white px-[24px] py-[16px] bg-[#186F65] sm:w-auto w-full capitalize mt-[16px] rounded  ">
+//            Book Ride
+//          </button>
+//        </div>
+//     </div>
+//    </div>
